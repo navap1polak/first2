e5ef3a5da50bc3289bc8c2a0d1b3268fb51ead5a
navap1polak <nava.igal@gmail.com>
2020-09-06 13:49:06 +0300

    first draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { JwPaginationComponent } from 'jw-angular-pagination';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JwPaginationComponent
   ],
   imports: [
     BrowserModule
